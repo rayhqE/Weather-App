b87882844d2a85b3eda15662046d88b7b618de4c
Rayyan <rayhq3@gmail.com>
2026-01-11 01:11:46 +0530

    Fetching of Data is Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const cityInput = document.getElementById("city-input");
   const getWeatherBtn = document.getElementById("get-weather-btn");
   const weatherInfo = document.getElementById("weather-info");
@@ -7,10 +7,36 @@ document.addEventListener("click", () => {
   const descriptionDisplay = document.getElementById("description");
   const errorDisplay = document.getElementById("error-message");
 
-  const API_KEY = a26bc9e04163f9ba702e8bf04f409729;
+  const API_KEY = "a26bc9e04163f9ba702e8bf04f409729";
 
-  getWeatherBtn.addEventListener("click", () => {
+  getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
     if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
   });
+  async function fetchWeatherData(city) {
+    //display the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("response:", response);
+    if (!response.ok) {
+      throw new Error("City Not Found");
+    }
+
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData() {}
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorDisplay.classList.remove("hidden");
+  }
 });
